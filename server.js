d517c9141a8d5803ee5a85d328d80e23e354402f
tboyd4 <tboyd1898@gmail.com>
2020-02-27 20:09:43 -0600

    reformatted code

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-
 const PORT = process.env.PORT || 5050;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 app.listen(PORT, () => {
-    console.log("Successfully listening on port : " + PORT);
-})
+  console.log("Successfully listening on port : " + PORT);
+});
