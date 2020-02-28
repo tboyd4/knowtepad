aeec1be7488e38895a736f1be1937f25694f385d
tboyd4 <tboyd1898@gmail.com>
2020-02-27 19:51:01 -0600

    created second route for notes.html

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
 app.listen(PORT, () => {
     console.log("Successfully listening on port : " + PORT);
 })
